5b06c08beecd933d069eeb2efd97a5744b867b4c
AMAN MONDAL <162113539+konlin008@users.noreply.github.com>
2024-10-12 21:19:08 +0530

    Add files via upload

--- ARRAY/easyProblems/checkIfSoted.py
+++ ARRAY/easyProblems/checkIfSoted.py
@@ -1,15 +1,13 @@
 arr = [1,2,3,4,5,6]
 arr2 = [3,4,5,1,2]
-08
+arr3 = [2,1,3,4]
 arr4 = [1,2,3]
 def checkIfSOrted(arr):
     count = 0
     for i in range(1,len(arr)):
         if arr[i]<arr[i-1]:
-            count +=1
-        if count>1:
             print("Array is not Sorted")
-            return
+            
     print("Array is Sorted")        
 checkIfSOrted(arr)
 checkIfSOrted(arr2)
