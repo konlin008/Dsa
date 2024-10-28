8e92c5b94c3469b8718cdae3b49f96abb08f5ce0
AMAN MONDAL <162113539+konlin008@users.noreply.github.com>
2024-10-28 13:35:12 +0530

    Add files via upload

---
+++ ARRAY/easyProblems/practice.py
@@ -0,0 +1,39 @@
+# def getSecondLargest( arr):
+#     largElm=arr[0]
+#     sLargElm= -1
+#     for elm in arr:
+#         if elm > largElm:
+#             sLargElm = largElm
+#             largElm = elm
+#         elif elm > sLargElm and elm<largElm:
+#             sLargElm = elm
+#     print(sLargElm)
+# arr = [10, 10]
+# getSecondLargest(arr)
+
+# arr = [1,2,3,4,5]
+# def checkarr(arr):
+#     for i in range(1,len(arr)):
+#         if arr[i] < arr[i-1]:
+#             print("Unsorted")
+#             return
+#     print("sorted")
+# checkarr(arr)
+
+nums = [3,4,5,1,2]
+nums1 = [2,1,3,4]
+nums2 = [1,2,3]
+
+def check(nums):
+    count = 0
+    n = len(nums)
+    for i in range(n):
+        if nums[i]> nums[(i+1) % n]:
+            count +=1
+            if count>1:
+                print(False)
+                return
+    print(True)
+check(nums)
+check(nums1)
+check(nums2)
