ee22ca8b6b24200023abca130299cedb0de92bf4
AMAN MONDAL <162113539+konlin008@users.noreply.github.com>
2024-11-05 11:52:20 +0530

    Add files via upload

--- ARRAY/easyProblems/practice.py
+++ ARRAY/easyProblems/practice.py
@@ -20,20 +20,45 @@
 #     print("sorted")
 # checkarr(arr)
 
-nums = [3,4,5,1,2]
-nums1 = [2,1,3,4]
-nums2 = [1,2,3]
+# nums = [3,4,5,1,2]
+# nums1 = [2,1,3,4]
+# nums2 = [1,2,3]
 
-def check(nums):
-    count = 0
-    n = len(nums)
-    for i in range(n):
-        if nums[i]> nums[(i+1) % n]:
-            count +=1
-            if count>1:
-                print(False)
-                return
-    print(True)
-check(nums)
-check(nums1)
-check(nums2)
+# def check(nums):
+#     count = 0
+#     n = len(nums)
+#     for i in range(n):
+#         if nums[i]> nums[(i+1) % n]:
+#             count +=1
+#             if count>1:
+#                 print(False)
+#                 return
+#     print(True)
+# check(nums)
+# check(nums1)
+# check(nums2)
+
+
+# arr=[0,0,1,1,1,2,2,3,3,4]
+
+# def removeDup(arr):
+#     i=0
+#     for j in range(len(arr)):
+#         if arr[j] != arr [i]:
+#             i+=1
+#             arr[i] = arr[j]
+          
+            
+#     print(arr[:i+1])
+    
+# removeDup(arr)
+
+arr = [1,2,3,4,5,6,7]
+
+def leftRoatedOne(arr):
+    temp= arr[0]
+    for i in range(1,len(arr)):
+        arr[i-1]=arr[i]
+    arr[len(arr)-1]= temp
+    print(arr)
+leftRoatedOne(arr)
